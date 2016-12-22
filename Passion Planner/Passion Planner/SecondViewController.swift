41767673b2b401db6419d8599832c169cb24f523
Brian Vo <ovnairb@gmail.com>
2016-12-22 02:15:43 -0800

    Initial commit for Planner App

---
+++ Passion Planner/Passion Planner/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Passion Planner
+//
+//  Created by Brian Vo on 12/22/16.
+//  Copyright © 2016 BryeIsa. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
